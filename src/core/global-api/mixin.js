420247edcd7aef80029cdfc1560cdfd623412aeb
snowy324 <wysnowy324@163.com>
2018-10-09 17:29:05 +0800

    add annotations to global-api/mixin.js

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -3,6 +3,7 @@
 import { mergeOptions } from '../util/index'
 
 export function initMixin (Vue: GlobalAPI) {
+  // mixin混入。传入一个Object对象，利用mergeOptions将mixin里传入的options都混入到Vue中。
   Vue.mixin = function (mixin: Object) {
     this.options = mergeOptions(this.options, mixin)
     return this
