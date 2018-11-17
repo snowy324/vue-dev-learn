b106929a8d07470ed95e768dcb206a3fa83713b5
snowy324 <wysnowy324@163.com>
2018-11-17 23:45:39 +0800

    add annotaitions to util/index.js

--- src/platforms/web/util/index.js
+++ src/platforms/web/util/index.js
@@ -8,6 +8,7 @@ export * from './element'
 
 /**
  * Query an element selector if it's not an element already.
+ * 函数返回document.querySelect(el)，返回该元素。如果没有该元素，则发出警告，并创建一个div。
  */
 export function query (el: string | Element): Element {
   if (typeof el === 'string') {
