c478138f4471ee96a82d8ff8237b23f70987d144
snowy324 <wysnowy324@163.com>
2018-11-17 20:33:03 +0800

    add annotations to scheduler.js

--- src/core/observer/scheduler.js
+++ src/core/observer/scheduler.js
@@ -14,6 +14,7 @@ export const MAX_UPDATE_COUNT = 100
 
 const queue: Array<Watcher> = []
 const activatedChildren: Array<Component> = []
+// 暂时不明确flow中这种写法的意义，等传入相应的值时再进行比对从而反推出这种写法的意义。
 let has: { [key: number]: ?true } = {}
 let circular: { [key: number]: number } = {}
 let waiting = false
@@ -24,9 +25,12 @@ let index = 0
  * Reset the scheduler's state.
  */
 function resetSchedulerState () {
+  // 重置index,queue,以及activatedChildren。
+  // 当直接设置array的length属性为0时，数组清空。
   index = queue.length = activatedChildren.length = 0
   has = {}
   if (process.env.NODE_ENV !== 'production') {
+    // 非生产环境重置circular。
     circular = {}
   }
   waiting = flushing = false
@@ -40,7 +44,11 @@ function flushSchedulerQueue () {
   let watcher, id
 
   // Sort queue before flush.
+  // 冲洗之前进行排序。
   // This ensures that:
+  // 保证组件的更新顺序总是从父组件到子组件。因为父组件总是在子组件之前创建。
+  // 保证组件的用户watcher总是比render watcher先执行，因为前者总是创建在后者之前。
+  // 如果组件在父组件watcher执行的时候销毁，它的watcher可以被跳过。
   // 1. Components are updated from parent to child. (because parent is always
   //    created before the child)
   // 2. A component's user watchers are run before its render watcher (because
@@ -51,17 +59,23 @@ function flushSchedulerQueue () {
 
   // do not cache length because more watchers might be pushed
   // as we run existing watchers
+  // 不缓存length因为可能在我们运行现存watcher的时候会有更多的watcher被push进来。
   for (index = 0; index < queue.length; index++) {
     watcher = queue[index]
     if (watcher.before) {
+      // before定义在watcher类中，是一个方法，暂时不明确功能是什么。
       watcher.before()
     }
     id = watcher.id
     has[id] = null
+    // 调用watcher的run方法。
     watcher.run()
     // in dev build, check and stop circular updates.
     if (process.env.NODE_ENV !== 'production' && has[id] != null) {
+      // 当has[id] != null的时候执行方法。不明确何时has[id]会出现不等于null的情况。
+      // circular[id]初始值为1，然后每次加1。
       circular[id] = (circular[id] || 0) + 1
+      // 如果circular[id]大于预设的MAX_UPDATE_COUNT = 100，就警告。
       if (circular[id] > MAX_UPDATE_COUNT) {
         warn(
           'You may have an infinite update loop ' + (
@@ -77,6 +91,7 @@ function flushSchedulerQueue () {
   }
 
   // keep copies of post queues before resetting state
+  // slice方法是生成一个新的数组，对原数组不产生影响。
   const activatedQueue = activatedChildren.slice()
   const updatedQueue = queue.slice()
 
@@ -89,6 +104,7 @@ function flushSchedulerQueue () {
   // devtool hook
   /* istanbul ignore if */
   if (devtools && config.devtools) {
+    // 如果devtools存在的话，发射flush方法。
     devtools.emit('flush')
   }
 }
@@ -99,6 +115,7 @@ function callUpdatedHooks (queue) {
     const watcher = queue[i]
     const vm = watcher.vm
     if (vm._watcher === watcher && vm._isMounted) {
+      // 调用组件的updated钩子。
       callHook(vm, 'updated')
     }
   }
