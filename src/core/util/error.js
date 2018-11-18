6e89b3e69440cffdd6da0d3177986491b249c2e2
snowy324 <wysnowy324@163.com>
2018-11-18 23:43:45 +0800

    add annotations to core/util/error.js

--- src/core/util/error.js
+++ src/core/util/error.js
@@ -4,14 +4,18 @@ import config from '../config'
 import { warn } from './debug'
 import { inBrowser, inWeex } from './env'
 
+// 处理错误函数(主要是为了执行errorCaptured钩子)。
 export function handleError (err: Error, vm: any, info: string) {
   if (vm) {
     let cur = vm
+    // 循环遍历当前组件vm的父级，检索option里的errorCaptured钩子。
     while ((cur = cur.$parent)) {
+      // 第一次循环的是cur（vm）的父级。也就意味着errorCaptured钩子是写在父级里，针对子组件的函数。
       const hooks = cur.$options.errorCaptured
       if (hooks) {
         for (let i = 0; i < hooks.length; i++) {
           try {
+            // errorCaptured钩子函数可以返回false，如果return了false，则不会继续往上执行。
             const capture = hooks[i].call(cur, err, vm, info) === false
             if (capture) return
           } catch (e) {
