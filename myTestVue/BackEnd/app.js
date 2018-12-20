765a3c498fbc69bf1f14878ee03617bd9cd98771
snowy324 <wysnowy324@163.com>
2018-12-20 18:36:22 +0800

    good shoot

--- myTestVue/BackEnd/app.js
+++ myTestVue/BackEnd/app.js
@@ -3,7 +3,7 @@ const Koa = require('koa')
 const static = require('koa-static')
 const app = new Koa()
 
-const staticPath = '../'
+const staticPath = '../FrontEnd/'
 
 app.use(static(
   path.join( __dirname,  staticPath)
